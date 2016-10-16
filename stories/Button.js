83bb7de07b198d0d0cf4f96db4b7e42a2c429cf1
Marcus R. Brown <me@igetgam.es>
2016-10-16 03:31:18 -0700

    Add several Button stories.

--- stories/Button.js
+++ stories/Button.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import Button from 'grommet/components/Button';
+import { Apps } from 'grommet/index-icons';
 import { storiesOf, action } from '@kadira/storybook';
+
+const commonProps = {
+  label: 'Button',
+  onClick: action('clicked')
+};
 
 const stories = [
   {
-    title: 'default',
+    title: 'Default',
     render() {
-      return <Button label="Button" onClick={action('clicked')} />
+      return <Button {...commonProps} />
+    }
+  },
+  {
+    title: 'Primary',
+    render() {
+      return <Button {...commonProps} primary />
+    }
+  },
+  {
+    title: 'Secondary',
+    render() {
+      return <Button {...commonProps} secondary />
+    }
+  },
+  {
+    title: 'Accent',
+    render() {
+      return <Button {...commonProps} accent />
+    }
+  },
+  {
+    title: 'Disabled',
+    render() {
+      return <Button label="Button" />
+    }
+  },
+  {
+    title: 'Icon',
+    render() {
+      return <Button {...commonProps} label="" icon={<Apps />} />
     }
   }
 ];

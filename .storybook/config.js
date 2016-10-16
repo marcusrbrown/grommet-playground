087b92eea2ffed3018e4c900d51faa4c18df97ec
Marcus R. Brown <me@igetgam.es>
2016-10-16 02:33:49 -0700

    Add the Grommet style and decorate stories with a Grommet App and Article.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,21 @@
-import { configure } from '@kadira/storybook';
+import { configure, addDecorator } from '@kadira/storybook';
+import React from 'react';
+import App from 'grommet/components/App';
+import Article from 'grommet/components/Article';
+import Box from 'grommet/components/Box';
 
 function loadStories() {
   require('../stories');
 }
+
+addDecorator(story => (
+  <App centered={false}>
+    <Article primary pad="large" align="center">
+      <Box pad="large" direction="row" align="center">
+        {story()}
+      </Box>
+    </Article>
+  </App>
+));
 
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -1,2 +1,3 @@
+import 'grommet/grommet.min.css';
 import './Welcome';
 import './Button';

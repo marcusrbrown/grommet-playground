a4f68bbb42508824060809c63c7112bc1cfce0cb
Marcus R. Brown <me@igetgam.es>
2018-01-15 16:21:53 -0700

    Add and use registerServiceWorker.js.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
+import registerServiceWorker from './registerServiceWorker';
 import 'grommet/grommet.min.css';
 
 ReactDOM.render(
   <Main />,
   document.getElementById('root')
 );
+registerServiceWorker();

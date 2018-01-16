0f1872bcb1fb220a9ef6686b38ac763841134594
Marcus R. Brown <me@igetgam.es>
2018-01-16 00:27:54 -0700

    Install the options Storybook addon and display the package name and version in the Storybook button.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 import Article from 'grommet/components/Article';
 import Box from 'grommet/components/Box';
+import Package from '../package.json';
 
 function loadStories() {
   require('../src/stories');
@@ -14,5 +16,9 @@ addDecorator(story => (
     </Box>
   </Article>
 ));
+
+setOptions({
+  name: `${Package.name} ${Package.version}`
+});
 
 configure(loadStories, module);

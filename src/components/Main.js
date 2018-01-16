32e821b0eb482113c798dce188adb403420b8d03
Marcus R. Brown <me@igetgam.es>
2018-01-15 23:47:05 -0700

    Fix linting issues in Main.js.

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,14 +10,14 @@ class Main extends Component {
   render() {
     return (
       <App>
-        <Header large justify="between" pad={{horizontal: 'medium'}}>
+        <Header size="large" justify="between" pad={{horizontal: 'medium'}}>
           <Title>Grommet Standalone</Title>
         </Header>
         <Box pad="medium">
           <Meter value={40} />
         </Box>
         <Footer primary appCentered direction="column" align="center" pad="small" colorIndex="grey-1">
-          <p>Build your ideas with <a href="http://grommet.io" target="_blank">Grommet</a>!</p>
+          <p>Build your ideas with <a href="http://grommet.io" target="_blank" rel="noopener noreferrer">Grommet</a>!</p>
         </Footer>
       </App>
     );

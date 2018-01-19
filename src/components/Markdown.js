6c78f163312c360de18ea8f586aa24f332210fad
Marcus R. Brown <me@igetgam.es>
2018-01-19 01:19:21 -0700

    Add support for highlight Markdown code blocks.

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import hljs from 'highlight.js/lib/highlight';
 import GrommetMarkdown from 'grommet/components/Markdown';
+import 'highlight.js/styles/github.css';
+
+// TODO: Replace this with react-highlight after it supports React 16+.
+// See: https://github.com/akiran/react-highlight/pull/43
+class Code extends Component {
+  highlightCode() {
+    const { className } = this.props;
+
+    if (className) {
+      const lang = className.replace('lang-', '');
+
+      hljs.registerLanguage(lang, require(`highlight.js/lib/languages/${lang}`));
+    }
+
+    // The <code> element is preceded by a <pre> element, so query the <pre>.
+    const nodes = this.el.parentNode.querySelectorAll('pre code');
+
+    for (let i = 0; i < nodes.length; ++i) {
+      hljs.highlightBlock(nodes[i]);
+    }
+  }
+
+  componentDidMount() {
+    this.highlightCode();
+  }
+
+  componentDidUpdate() {
+    this.highlightCode();
+  }
+
+  refEl = (el) => this.el = el;
+
+  render() {
+    const { className, children } = this.props;
+
+    return (
+      <code ref={this.refEl} className={className}>{children}</code>
+    );
+  }
+}
+
+const components = {
+  code: {
+    component: Code
+  }
+};
 
 const Markdown = ({ content }) => (
-  <GrommetMarkdown content={content} />
+  <GrommetMarkdown content={content} components={components} />
 );
 
 export default Markdown;

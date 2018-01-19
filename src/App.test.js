5a36c6e9b0cf55b97f5d345e9a051e11b31e1a45
Marcus R. Brown <me@igetgam.es>
2018-01-18 21:48:58 -0700

    Rename the Main component to App and move back into the source root.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './Main';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
+  ReactDOM.render(<App />, div);
 });
